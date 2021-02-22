106a41e38591dbb661ebd43999fc439b3a3e2105
conor <conorbrett72@gmail.com>
2021-02-22 20:11:34 +0000

    Connected to new database, fName and lName displaying

--- JDBC GUI/src/retrieve.java
+++ JDBC GUI/src/retrieve.java
@@ -8,36 +8,32 @@ import java.awt.event.*;
 public class retrieve {
 	
 	public static ResultSet rs;
-	public static String name="", email ="", lastname="";
+	public static String fName="", lName="";
 ///////////
 	
  public static void main(String args[]) {
  JFrame f = new JFrame();
-	JLabel nameLabel = new JLabel("Name: ");
-	JLabel emailLabel = new JLabel("Email: ");
-	JLabel lastNameLabel = new JLabel("Last Name: ");
-	final JTextField nameField = new JTextField(20);
-	final JTextField emailField = new JTextField(20);
-	final JTextField lastNameField = new JTextField(20);
+	JLabel fNameLabel = new JLabel("Name: ");
+	JLabel lNameLabel = new JLabel("Last Name: ");
+	final JTextField fNameField = new JTextField(20);
+	final JTextField lNameField = new JTextField(20);
 	JButton next = new JButton("Next");
 	JButton prev = new JButton("Prev");
 
 	
 	try{
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_create_db", "root", "password");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "password");
 		Statement st = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-		rs=st.executeQuery("select * from data");
+		rs=st.executeQuery("select * from test");
 	}
 		
 	catch(Exception e){}
 	
 		JPanel p = new JPanel(new GridLayout(4,2));
-		p.add(nameLabel);
-		p.add(nameField);
-		p.add(lastNameLabel);
-		p.add(lastNameField);
-		p.add(emailLabel);
-		p.add(emailField);
+		p.add(fNameLabel);
+		p.add(fNameField);
+		p.add(lNameLabel);
+		p.add(lNameField);
 		p.add(next);
 		p.add(prev);
 		f.add(p);
@@ -49,14 +45,12 @@ public class retrieve {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
 					if (rs.previous()){
-						name=rs.getString("name");
-						nameField.setText(name);
+						fName=rs.getString("fName");
+						fNameField.setText(fName);
 						
-						lastname=rs.getString("lastname");
-						lastNameField.setText(lastname);
+						lName=rs.getString("lName");
+						lNameField.setText(lName);
 				
-						email=rs.getString("email");
-						emailField.setText(email);
 				
 					}
 				} catch (SQLException e) {
@@ -71,14 +65,11 @@ public class retrieve {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
 					if (rs.next()){
-						name=rs.getString("name");
-						nameField.setText(name);
+						fName=rs.getString("fName");
+						fNameField.setText(fName);
 						
-						lastname=rs.getString("lastname");
-						lastNameField.setText(lastname);
-				
-						email=rs.getString("email");
-						emailField.setText(email);
+						lName=rs.getString("lName");
+						lNameField.setText(lName);
 				
 					}
 				} catch (SQLException e) {
